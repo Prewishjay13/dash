978754019f402c7bfc8645e3c899d542b92ad0b1
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2023-05-25 01:53:02 +0200

    .

---
+++ afstudeer/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../App.css';
+
+function Nav () {
+    return (
+   <nav className="nav-bar">
+    <ul className="nav-items">
+    <li>Home</li>
+    <li>Train</li>
+    <li>Models</li>
+    <li>Contact</li>
+   </ul>
+   </nav>
+
+    )
+}
+
+export default Nav;
