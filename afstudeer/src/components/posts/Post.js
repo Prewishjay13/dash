219519126dc42ac580479c11f8260f0d52ecf9a3
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2023-06-01 01:23:16 +0200

    Update Post.js

--- afstudeer/src/components/posts/Post.js
+++ afstudeer/src/components/posts/Post.js
@@ -1,5 +1,9 @@
 import React from "react"
 
+//regel 8 if statement als titels bestaat render de div met de titel extra vb:
+// {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+//als openSpots = 0 render de div 
+
 function Post(props) {
     return (
         <div>
