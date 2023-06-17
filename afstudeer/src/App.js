651464c3ed0eb99a58ba29eca45e314f201cde32
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2023-06-17 02:41:22 +0200

    headers & values
    test to get headers from a csv and display them on screen(web browser)

--- afstudeer/src/App.js
+++ afstudeer/src/App.js
@@ -3,18 +3,12 @@ import CsvUpload from './components/Csvtest';
 
 function App() {
 
-  const handleBackClick = () => {
-    // Handle back click logic
-  };
 
-  const handleSubmit = (csvData) => {
-    // Handle submit logic
-  };
   return (
     <div className="App">
 
 
-<CsvUpload handleBackClick={handleBackClick} onSubmit={handleSubmit} />
+<CsvUpload/>
     </div>
   );
 }
