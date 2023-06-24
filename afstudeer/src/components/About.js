27d271b8274908f177ca297529aa45adcdae576b
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2023-06-24 19:05:43 +0200

    aboutpage

---
+++ afstudeer/src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About () {
+    return(
+        <h>About</h>
+    )
+}
+
+export default About;
--- afstudeer/src/components/Nav.js
+++ afstudeer/src/components/Nav.js
@@ -3,7 +3,7 @@ import {Routes, Route, Link} from 'react-router-dom'
 import Home from './Home';
 import Models from './Models';
 import Posts from './Posts';
-
+import About from './About';
 function Nav() {
 
     return (
@@ -13,11 +13,13 @@ function Nav() {
                     <Link to="/" className="nav-item">Home</Link>
                     <Link to="/models" className="nav-item">Models</Link>
                     <Link to="/posts" className="nav-item">Posts</Link>
+                    <Link to="/about" className="nav-item">About</Link>
                 </nav>
                 <Routes>
                     <Route path="/" element={<Home />}/>     
                     <Route path="/models" element={<Models />}/>
                     <Route path="/posts" element={<Posts />}/>
+                    <Route path="/about" element={<About />}/>
                 </Routes>
             </div>
     );
