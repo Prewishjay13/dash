b4c894f3f7932895b86dd3231ece11b8b7bac006
Prewishjay13 <0935796@hr.nl>
2023-07-01 01:44:25 +0200

    Update CsvUpload.js

--- afstudeer/src/components/CsvUpload.js
+++ afstudeer/src/components/CsvUpload.js
@@ -1,7 +1,7 @@
 import React from 'react';
-const csv = require('csv-parser')
-const fs = require('fs')
-const results = [];
+//const csv = require('csv-parser')
+// const fs = require('fs')
+// const results = [];
 
 
 
@@ -10,12 +10,12 @@ const results = [];
 
   export default function Upload() {
 
-    fs.createReadStream('data.csv')
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-      console.log(results);
-    });
+    // fs.createReadStream('data.csv')
+    // .pipe(csv())
+    // .on('data', (data) => results.push(data))
+    // .on('end', () => {
+    //   console.log(results);
+    // });
    
     return (
             <div>
