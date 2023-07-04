d81be0855d1d1e33418059a212b40a0f1e0bc404
Prewishjay13 <0935796@hr.nl>
2023-07-04 10:46:43 +0200

    .

--- afstudeer/src/components/Nav.js
+++ afstudeer/src/components/Nav.js
@@ -4,10 +4,9 @@ import Home from '../pages/Home';
 import Charts from '../pages/Charts';
 import Models from '../pages/Models';
 import Knn from './Knn';
-
 import About from '../pages/About';
 import Table from './Csvtable';
-import Csv from './Csvtest';
+//import Csv from './Csvtest';
 import Upload from './CsvUpload';
 import Tensor from './Tensor';
 import Scatterplot from './Scatterplot';
@@ -26,7 +25,7 @@ function Nav() {
                     <li><Link to="/models" className="nav-item">Models</Link></li>
                   
                     <li><Link to="/about" className="nav-item">About</Link></li>
-                    <li><Link to="/test" className="nav-item">Test</Link></li>
+                    
                     <li><Link to="/table" className="nav-item">Table</Link></li>
                     <li><Link to="/upload" className="nav-item">Upload</Link></li>
                    
@@ -42,7 +41,7 @@ function Nav() {
                  
                     <Route path="/about" element={<About />}/>
                     <Route path="/table" element={<Table />}/>
-                    <Route path="/test" element={<Csv />}/>
+                    
                     <Route path="/upload" element={<Upload />}/>
                     <Route path="/scatterplot" element={<Scatterplot />}/>
                     <Route path="/lineplot" element={<Lineplot />}/>
